1594fe12a6f8ee36a86e27ebaa28307fc117dd84
GhassenEljday <ghasseneljday@gmail.com>
2023-03-18 00:00:05 +0100

    Update structure

---
+++ index.ts
@@ -0,0 +1,6 @@
+import GooglePlayButton from "./src/components/GooglePlayButton";
+import AppGalleryButton from "./src/components/AppGalleryButton";
+import AppStoreButton from "./src/components/AppStoreButton";
+import ButtonsContainer from "./src/components/ButtonsContainer";
+
+export { GooglePlayButton, AppGalleryButton, AppStoreButton, ButtonsContainer };
